93c7db05ed84996845e04fa019c59afdc110a7bc
Cihat <44507004+yigitcihat@users.noreply.github.com>
2022-10-24 15:21:13 +0300

    WindowsBuildAdded

--- Assets/Scripts/Data/Runtime/ProductionDraggedItem.cs
+++ Assets/Scripts/Data/Runtime/ProductionDraggedItem.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class ProductionDraggedItem
 {
